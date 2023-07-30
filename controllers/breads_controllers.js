247051f5dcc1cb946ac7b742c5479b13d988d8a3
Jose Rodriguez <joserodriguez@Joses-MBP-2.home>
2023-07-29 17:03:27 -0700

    added nodemon to scirpt start

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -2,10 +2,10 @@ const express = require('express');
 const breads = express.Router();
 const Bread = require('../models/bread.js');
 
-// SHOW
+// SHOWS ON SERVER
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })  
+    res.send(Bread[req.params.arrayIndex]);
+  });  
 
 module.exports = breads;
 
